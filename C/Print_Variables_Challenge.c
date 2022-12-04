1aa079cfb1d39d9f3110ebc399a6ae31c48e76db
VictorBK <victorbrian4@gmail.com>
2022-12-04 08:55:18 +0300

    Practice on Variables

---
+++ C/Print_Variables_Challenge.c
@@ -0,0 +1,19 @@
+// Problem Description
+// Create a program to print the variable.
+      // Create two variables number1 with value 34 and number2 with value 78.32.
+      // Print number1 and number2 with a space in between.
+#include <stdio.h>
+
+int main() {
+  
+    // create integer variable
+    int num1 = 34;
+
+    // create double variable
+    double num2 = 78.32;
+
+    // print variables
+    printf("%d", num1);
+    printf(" %lf", num2);
+return 0;
+}
