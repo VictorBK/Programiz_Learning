eca3042dcf777f413527a99a651a2339f7d848c9
VictorBK <victorbrian4@gmail.com>
2022-12-06 01:14:35 +0300

    Assigning Value to double and char Variables

---
+++ C/assign_value.c
@@ -0,0 +1,18 @@
+// Create a program to assign values to double and char variables.
+// Create a double variable named number with value 86.99.
+// Create a character variable named letter with value 'z'.
+// Print both variables with two spaces between them.
+
+#include <stdio.h>
+
+int main() {
+
+  double number = 86.99;
+
+  char letter = 'z';
+
+  printf("%lf ", number);
+  printf(" %c", letter);
+
+  return 0;
+}
