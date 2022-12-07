b274d96a3b6ec3a2f488f83501027d3ec415390a
VictorBK <victorbrian4@gmail.com>
2022-12-07 09:08:55 +0300

    Changing value of a variable

---
+++ C/change_value.c
@@ -0,0 +1,22 @@
+// CHANGING VALUES OF VARIABLES
+// Create a program to change the value of a variable.
+      // Create an integer variable number with value 31.
+      // Print the variable.
+      // Change the value of the variable to 99.
+      // Print the variable again with a space before it.
+
+#include <stdio.h>
+
+int main() {
+  
+    // create and print an integer variable
+    int number = 31;
+    printf("%d ", number);
+
+    // change the value of variable
+    int number2 = 99;
+    printf("%d", number2);
+
+    return 0;
+}
+
